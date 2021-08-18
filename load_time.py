a4c3ffb5b6aec917b340ff49c9e72d11f5a1c19f
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2021-08-18 08:35:24 -0400

    Update load_time.py

--- load_time.py
+++ load_time.py
@@ -3,11 +3,12 @@ start_time = time.time()
 import h5py
 import numpy as np
 import torch
+start_time2 = time.time()
 
 file_path = './data/train_shapes.h5'
 f = h5py.File(file_path, 'r')
 data = np.array(f['data'])
 labels = np.array(f['label'])
-print("cpu load time",time.time() - start_time)
+print("cpu load time",time.time() - start_time2)
 val = torch.from_numpy(data).cuda()
 print("gpu load time",time.time() - start_time)
